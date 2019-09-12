82027b0750cf07dfee3e6c7c07991a8c43644d73
songguojun <gj_yxcom@163.com>
2019-09-12 17:42:09 +0800

    修改读文件路径

--- routes/index.js
+++ routes/index.js
@@ -7,24 +7,24 @@ const md = require('markdown-it')();
 const router = require('koa-router')()
 
 router.get('/', async (ctx, next) => {
-  await ctx.render( 'index' , { title: '前端俱乐部' })
+  await ctx.render('index', { title: '前端俱乐部' })
 })
 
 router.get('/activity', async (ctx, next) => {
-  const filepath = await path.join(__dirname,'../','/public/upload/md/','test.md');
-  var mdData = '';
+  let mdData = '';
+  let filepath = await path.join('./public/upload/md/', 'test.md');
   await new Promise((resolve, reject) => { // 读image文件夹
-    fs.readFile( filepath , "utf8" , (err, data) => {
-      if(err) { console.log(err); return false; }
+    fs.readFile(filepath, "utf8", (err, data) => {
+      if (err) { console.log(err); return false; }
       //mdData = md.render(data);
       mdData = data;
       resolve()
     });
   });
-  await ctx.render( 'activity' , { title: '详情页面',mdData: mdData })
+  await ctx.render('activity', { title: '详情页面', mdData: mdData })
 })
 router.get('/write', async (ctx, next) => {
-  await ctx.render( 'write' , { title: '发布文章' })
+  await ctx.render('write', { title: '发布文章' })
 })
 
 router.get('/json', async (ctx, next) => {
