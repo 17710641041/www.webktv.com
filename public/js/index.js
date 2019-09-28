d5145714e71f62dbe4b6c13a70c31ae6234d620a
songguojun <gj_yxcom@163.com>
2019-09-28 11:28:02 +0800

    样式修改

--- public/js/index.js
+++ public/js/index.js
@@ -1,53 +1,38 @@
 
-var index = 1;
-var isGet = true;
-var str = ''
+// var index = 1;
+// var isGet = true;
+// var str = ''
 
-$(window).scroll(function () {
-  var viewHeight = $(window).height();//可见高度
-  var contentHeight = $(".list").get(0).scrollHeight + 460;//内容高度
-  var scrollHeight = $(document).scrollTop();//滚动高度
-  if (contentHeight - viewHeight - scrollHeight <= 0) {
-    if (isGet) {
-      $(".jzIcon").show();
-      ++index;
-      $.ajax({
-        type: "GET",
-        url: "/api/v1/article/get",
-        data: {
-          index: index
-        },
-        success: function (msg) {
-          console.log(msg);
-          if (msg.length < 10) {
-            isGet = false;
-          }
-          for (var i = 0; i < msg.length; i++) {
-            str += `
-              <a class="list-items" href="/activity?id=${msg[i].address}">
-                <div class="list-items-title">${msg[i].title}</div>
-                <div class="list-items-foot">
-                  <div class="users">
-                    <div class="user-info">
-                      <span class="name">作者: ${msg[i].u.nickname}</span>
-                      <span class="name">时间: ${msg[i].createdAt}</span>
-                    </div>
-                  </div>
-                </div>
-              </a>
-            `
+//  $.ajax({
+//         type: "GET",
+//         url: "/api/v1/article/get",
+//         data: {
+//           index: index
+//         },
+//         success: function (msg) {
+//           console.log(msg);
+//           if (msg.length < 10) {
+//             isGet = false;
+//           }
+//           for (var i = 0; i < msg.length; i++) {
+//             str += `
+//               <a class="list-items" href="/activity?id=${msg[i].address}">
+//                 <div class="list-items-title">${msg[i].title}</div>
+//                 <div class="list-items-foot">
+//                   <div class="users">
+//                     <div class="user-info">
+//                       <span class="name">作者: ${msg[i].u.nickname}</span>
+//                       <span class="name">时间: ${msg[i].createdAt}</span>
+//                     </div>
+//                   </div>
+//                 </div>
+//               </a>
+//             `
 
-          }
-          $(".list").append(str);
-          $(".jzIcon").hide();
-        }
-      });
-    }
-  } else {
-    $(".jzTexe").show();
-  }
-
-});
+//           }
+//           $(".list").append(str);
+//         }
+//  });
 
 
 
